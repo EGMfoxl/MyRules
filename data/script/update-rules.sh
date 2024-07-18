eabfb2e6763a43c0d0e8487fc959df97f18a8c07
EGMfoxl <121678224+EGMfoxl@users.noreply.github.com>
2024-07-18 21:54:43 +0800

    Update update-rules.sh

--- data/script/update-rules.sh
+++ data/script/update-rules.sh
@@ -17,7 +17,7 @@ cd tmp
 echo '下载规则'
 rules=(
   "https://small.oisd.nl/" #oisd规则
-  "https://easylist.to/easylist/easylist.txt
+  "https://easylist.to/easylist/easylist.txt"
   "https://easylist-downloads.adblockplus.org/easylistchina.txt"
   "https://raw.githubusercontent.com/afwfv/DD-AD/main/rule/dns.txt" #afwfv  
   "https://raw.githubusercontent.com/TG-Twilight/AWAvenue-Ads-Rule/main/AWAvenue-Ads-Rule.txt" #秋风
